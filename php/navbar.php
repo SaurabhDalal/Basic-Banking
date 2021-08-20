2f0c6ca654c03a5fc44a5ad0f73c32ab86e6100d
unknown <saurabhdalal98@gmail.com>
2021-08-20 18:27:11 +0530

    initial commit

---
+++ php/navbar.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <style>
+    a:link {
+      color: white;
+    }
+
+    a:hover {
+      color: white;
+      text-decoration: none;
+    }
+    .cyan {
+      background-color: #8f58c9  !important;
+  }
+
+  </style>
+</head>
+
+<body>
+  <!--Responsive Navbar-->
+  <nav>
+    <div class="container-fluid nav-wrapper cyan z-depth-5">
+      <a href="../index.php" class="brand-logo"><img src="../images/BankLogo.png" style="height:45px;width:50px" /></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="transfermoney.php">View Customers</a></li>
+        <li><a href="transactionhistory.php">Transactions</a></li>
+        <li><a href="../index.php">Home</a></li>
+      </ul>
+    </div>
+  </nav>
+  <!--end of navbar-->
+</body>
+
+</html>
